f8f1500d823b45b0ff0377813263d162cdd689e6
Sai Kiran Veeraneni <veeranenisaikiran@gmail.com>
2020-05-04 16:27:37 +0530

    Updates

---
+++ precache-manifest.3a206c47f0ebd321ed73a63424a4b337.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93caad85db1d598d2820aae859f7ceb",
+    "url": "/shortly/index.html"
+  },
+  {
+    "revision": "65f205f6a8ff8388c913",
+    "url": "/shortly/static/css/main.a2b43536.chunk.css"
+  },
+  {
+    "revision": "a3df41ae68911de42257",
+    "url": "/shortly/static/js/2.7f3f7dc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shortly/static/js/2.7f3f7dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f205f6a8ff8388c913",
+    "url": "/shortly/static/js/main.ba57b327.chunk.js"
+  },
+  {
+    "revision": "54dcf06c8bcbd48f65f4",
+    "url": "/shortly/static/js/runtime-main.7dfd2374.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/shortly/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/shortly/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/shortly/static/media/brand.1922b68a.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/shortly/static/media/customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/shortly/static/media/hero.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/shortly/static/media/logo.6387840f.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/shortly/static/media/records.e3d9caa9.svg"
+  }
+]);
